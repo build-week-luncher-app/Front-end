a1f8c6d6906e4a072f8d9a11cd7eb326b3c3b34c
Natalie McCroy <nataliemccroy@gmail.com>
2019-07-26 21:42:51 -0500

    working on login form

--- luncher/src/App.js
+++ luncher/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 
 import DonorHomePage from '../src/components/DonorHomePage/DonorHomePage';
+import LoginForm from '../src/components/LoginForm/LoginForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <DonorHomePage />
+      <LoginForm />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
