b94ad05e4315d2bb6ca49a1ac111898e634facce
Cobrettie <cobrettiegarner3@gmail.com>
2019-07-30 06:50:08 -0400

    building navbar

--- luncher/src/App.js
+++ luncher/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 
+import NavMenu from './components/NavMenu/NavMenu'
 import DonorHomePage from '../src/components/DonorHomePage/DonorHomePage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavMenu />
       <DonorHomePage />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- luncher/src/components/NavMenu/NavMenu.js
+++ luncher/src/components/NavMenu/NavMenu.js
@@ -1,1 +1,18 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'
+import {
+    Navbar,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap'
+
+const NavMenu = () => {
+    return (
+        <div className="App">
+            
+        </div>
+    );
+}
+
+export default NavMenu
