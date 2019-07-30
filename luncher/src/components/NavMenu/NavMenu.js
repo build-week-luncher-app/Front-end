b8147f22d79bccc50a24f07b5a7a4dc61e0fa914
Cobrettie <cobrettiegarner3@gmail.com>
2019-07-30 06:55:48 -0400

    basic NavMenu added, fully responsive

--- luncher/src/components/NavMenu/NavMenu.js
+++ luncher/src/components/NavMenu/NavMenu.js
@@ -1,18 +1,50 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 import {
-    Navbar,
-    Nav,
-    NavItem,
-    NavLink,
-} from 'reactstrap'
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-const NavMenu = () => {
+export default class NavMenu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
     return (
-        <div className="App">
-            
-        </div>
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">LUNCHER</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/">Schools</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     );
+  }
 }
-
-export default NavMenu
