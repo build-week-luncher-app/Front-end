63e7208f729fe79767ba6f8e15e4dceffc85fe06
Cobrettie <cobrettiegarner3@gmail.com>
2019-07-31 19:31:58 -0400

    Revert "update App.js"
    This reverts commit 0c8a28b2d64fe234ad4f0fd8bf14f8ed5fca359d.

--- luncher/src/App.js
+++ luncher/src/App.js
@@ -1,34 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom'
 
 import DonorHomePage from '../src/components/DonorHomePage/DonorHomePage';
 import './App.css';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      schools: []
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Route exact path="/" component={DonorHomePage} />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <Route 
+        exact path ="/" component={DonorHomePage}
+      />
+    </div>
+  );
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Route 
-//         exact path ="/" component={DonorHomePage}
-//       />
-//     </div>
-//   );
-// }
-
-// export default App
+export default App
