0c8a28b2d64fe234ad4f0fd8bf14f8ed5fca359d
Cobrettie <cobrettiegarner3@gmail.com>
2019-07-31 19:25:14 -0400

    update App.js

--- luncher/src/App.js
+++ luncher/src/App.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 
 import DonorHomePage from '../src/components/DonorHomePage/DonorHomePage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Route 
-        exact path ="/" component={DonorHomePage}
-      />
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      schools: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/" component={DonorHomePage} />
+      </div>
+    )
+  }
 }
 
-export default App
+// function App() {
+//   return (
+//     <div className="App">
+//       <Route 
+//         exact path ="/" component={DonorHomePage}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App
