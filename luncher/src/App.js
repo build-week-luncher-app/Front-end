456d95a756bdf5133971db26b56ab7e2aa17d600
Cobrettie <cobrettiegarner3@gmail.com>
2019-07-31 20:29:22 -0400

    remove routing to test netlify deployment

--- luncher/src/App.js
+++ luncher/src/App.js
@@ -4,14 +4,22 @@ import { Route } from 'react-router-dom'
 import DonorHomePage from '../src/components/DonorHomePage/DonorHomePage';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <Route 
+//         exact path ="/" component={DonorHomePage}
+//       />
+//     </div>
+//   );
+// }
+
 function App() {
   return (
     <div className="App">
-      <Route 
-        exact path ="/" component={DonorHomePage}
-      />
+      <DonorHomePage />
     </div>
-  );
+  )
 }
 
 export default App
--- luncher/src/index.js
+++ luncher/src/index.js
@@ -6,10 +6,15 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <App />
+//     </BrowserRouter>,
+//     document.getElementById('root')
+// );
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <App />,
     document.getElementById('root')
 );
 
